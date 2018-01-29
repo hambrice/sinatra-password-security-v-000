002a925240417b5007fa96525e4a1fcabae4dfd4
Brad Hambrice <hambrice@uab.edu>
2018-01-29 07:44:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ if user.save
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
- 
+
     if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect "/success"
